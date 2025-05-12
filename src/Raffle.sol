2c0871f649c9c8015afd3cc26559376b95ecf57d
okweb-3 <fortwu06@gmail.com>
2025-05-12 23:22:09 +0800

    Finish Creating custom errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,25 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 /**
@@ -6,12 +28,22 @@ pragma solidity ^0.8.18;
  * @notice This contract is for learning purposes only
  * @dev Implemets ChainLink VRFv2.5
  */
+
 contract Raffle {
+    // errors
+    error Raffle__Raffle__SenderMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough funds!");
+        // require(msg.value >= i_entranceFee, SenderMoreToEnterRaffle（）);
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SenderMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
     /** Getter Function */
