dbaa3045f94166ad8d6a0622e78e57e7e3dfc274
okweb-3 <fortwu06@gmail.com>
2025-05-14 20:25:24 +0800

    Finish Implement the Chainlink VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,13 +29,46 @@ pragma solidity ^0.8.18;
  * @dev Implemets ChainLink VRFv2.5
  */
 
-contract Raffle {
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+contract Raffle is VRFConsumerBaseV2 {
     // errors
-    error Raffle__Raffle__SenderMoreToEnterRaffle();
+    error Raffle__SenderMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
-    constructor(uint256 entranceFee) {
+    //@dev The duration of the lottery period in seconds
+    uint256 private immutable i_interval;
+    //玩家数组
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    // address immutable i_vrfCoordinator;
+
+    //chainlink VRF realted variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint256 private immutable i_subscriptionId;
+    uint256 private constant REQUSER_CONFIRMARIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
+    event RaffleEntered(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
     function enterRaffle() public payable {
         // require(msg.value >= i_entranceFee, "Not enough funds!");
@@ -43,9 +76,38 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle__SenderMoreToEnterRaffle();
         }
+        //将当前合约调用的地址添加到splayers数组中。
+        s_players.push(payable(msg.sender));
+        //使得迁移更容易，使得索引更容易
+        // 玩家成功进入抽奖时触发的事件
+        emit RaffleEntered(msg.sender);
     }
+    //获取随机数
+    //使用随机数
+    //自动调用
+    //
+    function pickWinner() public {
+        // s_requestId = s_vrfCoordinator.requestRandomWords(
+        //     VRFV2PlusClient.RandomWordsRequest({
+        //         keyHash: keyHash,
+        //         subId: s_subscriptionId,
+        //         requestConfirmations: requestConfirmations,
+        //         callbackGasLimit: callbackGasLimit,
+        //         numWords: numWords,
+        //         extraArgs: VRFV2PlusClient._argsToBytes(
+        //             VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+        //         )
+        //     })
+        // );
 
-    function pickWinner() public {}
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
     /** Getter Function */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
