2fe5ac893f15842eb081bcabf6673b3f08f276cb
okweb-3 <fortwu06@gmail.com>
2025-05-25 15:52:55 +0800

    Finish Test and Refactoring the PerformUpkeep

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -85,4 +85,36 @@ contract RaffleTest is Test {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: entranceFee}();
     }
+    /*//////////////////////////////////////////////////////////////
+                            PERFORM UPKEEPER
+    //////////////////////////////////////////////////////////////*/
+    function testPerformUpkeeperCanOnlyRunIfCheckUpKeepIsTrue()public{
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        //Act
+        //Assert
+        raffle.performUpkeep("");
+    }
+    function testPerformUpkeeperRevertsIfCheckUpkeepIsFalse()public{
+        //Arrange
+        uint256 currentBalance=0;
+        uint256 numPlayers=0;
+        Raffle.RaffleState rState=raffle.getRaffleState();
+
+
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        currentBalance = currentBalance+entranceFee;
+        numPlayers = 1;
+        //Act
+        //Assert
+        vm.expectRevert(abi.encodeWithSelector(Raffle.Raffle__UpkeepNotNeed.selector,currentBalance,numPlayers,rState));
+        raffle.performUpkeep("");
+
+    }
+
 }
